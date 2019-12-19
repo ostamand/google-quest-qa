1803644dd95e8a46e00055476dc19541820a8de7
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2019-12-19 12:04:20 -0500

    Add run train on questions script

---
+++ scripts/run_train_bert_on_questions.sh
@@ -0,0 +1,12 @@
+(cd ..; python3 train_bert_on.py \
+        --do_apex \
+        --do_wandb \
+        --maxlen 512 \
+        --bs 4 \
+        --accumulation_steps 2 \
+        --dp 0.2 \
+        --lr1 1e-2 \
+        --fold 0 \
+        --out_dir outputs/bert_on_questions)
+# TODO:
+# - Try dp 0.2
