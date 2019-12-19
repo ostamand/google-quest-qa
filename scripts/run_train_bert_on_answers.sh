37d011a76de6b9efbcdabc78bded8fba15b4af5e
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2019-12-19 11:54:13 -0500

    Add training script on answers

---
+++ scripts/run_train_bert_on_answers.sh
@@ -0,0 +1,14 @@
+(cd ..; python3 train_bert_on.py \
+        --do_apex \
+        --do_wandb \
+        --maxlen 512 \
+        --bs 4 \
+        --accumulation_steps 2 \
+        --dp 0.2 \
+        --lr1 1e-3 \
+        --fold 0 \
+        --do_answer \
+        --out_dir outputs/bert_on_answers)
+# TODO: 
+# --lr1 1e-3
+# --dp 0.2
