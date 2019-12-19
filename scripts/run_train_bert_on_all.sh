82acfeef8c0fd6f1dc730649ec6fab8bdf26e34f
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2019-12-19 15:17:13 -0500

    Add training script

---
+++ scripts/run_train_bert_on_all.sh
@@ -0,0 +1,11 @@
+(cd ..; python3 train_bert_on_all.py \
+        --do_apex  \
+        --do_wandb \
+        --bs 4 \
+        --fold 0 \
+        --out_dir outputs/test_on_all \
+        --dp 0.1 \
+        --bert_wd 0.01 \
+        --model_dir model/bert-base-uncased \
+        --warmup 0.5 \
+        --warmdown 0.5)
